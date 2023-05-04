15d21efcd3a51536bc4156f6f74497491bcd9c38
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2023-05-04 14:20:39 -0300

    Update winstall-8280.bat

--- winstall-8280.bat
+++ winstall-8280.bat
@@ -5,4 +5,3 @@ rem Instalando arquivo padrao com libre
 winget install --id=7zip.7zip -e  && winget install --id=Adobe.Acrobat.Reader.64-bit -e  && winget install --id=Google.Chrome.Canary -e  && winget install --id=Mozilla.Firefox -e  && winget install --id=TheDocumentFoundation.LibreOffice -e  && winget install --id=SlackTechnologies.Slack -e 
 if %ERRORLEVEL% EQU 0 echo Instalado com sucesso.
 pause
-exit
